03427b18fed0ba7fd35c4d1a5535cdee5c8d43fb
Samir Tiwari <samirtiwari2061@gmail.com>
2024-06-11 13:31:48 +0545

    fix order page bug

--- client/src/pages/Orders.tsx
+++ client/src/pages/Orders.tsx
@@ -1,7 +1,6 @@
 import { ReactElement, useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import { Column } from "react-table";
 import { Skeleton } from "../components/Loader";
 import TableHOC from "../components/admin/TableHOC";
